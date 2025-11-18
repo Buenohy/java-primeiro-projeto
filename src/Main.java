75945ad5455ebe225651074e2afeee0612bc231c
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-18 04:36:37 -0300

    feat: Text Block with Java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Alô Mundo!");
-        System.out.printf("Filme: Top Gun: Maverick");
+        System.out.println("Filme: Top Gun: Maverick");
 
         int anoDeLancamento = 2022;
-        System.out.printf("Ano de lançamento" + anoDeLancamento);
+        System.out.println("Ano de lançamento" + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                MUito bom!
+                Ano de Lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
     }
 }
