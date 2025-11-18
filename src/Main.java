7dda4c4af3dd7a40986ee49aad6cf95f188f81fe
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-18 04:25:18 -0300

    feat: using variables with Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Alô Mundo!");
+        System.out.printf("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.printf("Ano de lançamento" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
     }
 }
